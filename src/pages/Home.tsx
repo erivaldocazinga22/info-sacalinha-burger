bd6e68f072f222c637dcbc96fe192ac3364076a8
JoaoSacala <joaosacala88@gmail.com>
2025-06-28 10:26:11 +0100

    style: improve code formatting and consistency in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,13 @@ import { Header } from "../components/layout/Header";
 import { Menu } from "../components/layout/Menu";
 
 export default function Home() {
-    return (
-        <main className="min-h-screen relative overflow-hidden">
-            <AnimatedBackground />
-            <div className="relative z-10 px-4 mb-10">
-                <Header />
-                <Menu />
-            </div>
-        </main>
-  
-    );
+	return (
+		<main className="min-h-screen relative overflow-hidden">
+			<AnimatedBackground />
+			<div className="relative z-10 px-4 mb-10">
+				<Header />
+				<Menu />
+			</div>
+		</main>
+	);
 }
