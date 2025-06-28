5c425c2ef2d5f915cfd818f2e630c77e3af18a55
JoaoSacala <joaosacala88@gmail.com>
2025-06-28 10:22:43 +0100

    style: improve code formatting and consistency in DarkModeToggle component

--- src/components/layout/darkModeToggle.tsx
+++ src/components/layout/darkModeToggle.tsx
@@ -5,11 +5,18 @@ import { cn } from "../../lib/utils";
 
 type DarkModeToggleProps = React.ComponentProps<"button">;
 
-export const DarkModeToggle = ({ className, ...rest}: DarkModeToggleProps) => {
-    const [isDarkMode, toggleDarkMode] = useDarkMode(); 
-    return ( 
-        <button onClick={toggleDarkMode} className={cn("p-2 rounded-full text-neutral-900 dark:text-neutral-50 bg-neutral-200 dark:bg-neutral-800", className)} {...rest}> 
-            {isDarkMode ? <IconSun /> : <IconMoon />} 
-        </button> 
-    );
+export const DarkModeToggle = ({ className, ...rest }: DarkModeToggleProps) => {
+	const [isDarkMode, toggleDarkMode] = useDarkMode();
+	return (
+		<button
+			onClick={toggleDarkMode}
+			className={cn(
+				"p-2 rounded-full text-neutral-900 dark:text-neutral-50 bg-neutral-200 dark:bg-neutral-800",
+				className,
+			)}
+			{...rest}
+		>
+			{isDarkMode ? <IconSun /> : <IconMoon />}
+		</button>
+	);
 };
